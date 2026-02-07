497385f49c1d4e6ec82ba7b96eca97e2f57e0feb
Himanshu gupta <guptahimu90@gmail.com>
2026-02-07 13:37:18 +0530

    Initial commit

---
+++ app/src/api/test/route.ts
@@ -0,0 +1,10 @@
+import dbConnect from "../../lib/dbConnect";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await dbConnect();
+
+  return NextResponse.json({
+    message: "DB connected successfully 🚀",
+  });
+}
