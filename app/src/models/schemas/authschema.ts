e25f4b7b3d6c575f47cd6563fa7a3830877d1ddd
Himanshu gupta <guptahimu90@gmail.com>
2026-02-14 13:09:22 +0530

    Initial commit

--- app/src/models/schemas/authschema.ts
+++ app/src/models/schemas/authschema.ts
@@ -15,6 +15,15 @@ export const signupSchema = z
   .object({
     name: z
       .string()
+
+
+
+
+
+
+
+
+      
       .trim()
       .min(2, "Name must be at least 2 characters"),
 
@@ -56,7 +65,7 @@ export const signupSchema = z
 
 
 
-    
+
   });
 
 // ================= TYPE =================
